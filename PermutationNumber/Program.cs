1b59167afed2c06550ad0f44e5231b629ecb34e9
karimdevwm <ka.trabelsi9@gmail.com>
2021-05-11 18:17:34 +0200

    Ajoutez des fichiers projet.

---
+++ PermutationNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwappingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number for n1 :");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a number for n2 :");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("before");
+            
+            n1 = n1 + n2;
+            n2 = n1 - n2;
+            n1 = n1 - n2;
+
+            Console.WriteLine("after");
+
+            Console.WriteLine("n1 = "+n1);
+            Console.WriteLine("n2 = "+n2);
+        }
+    }
+}
